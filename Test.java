a157e4b0ade6b7a57d8549895763949083fb09ed
Makanta Luckson <2017006974@student.unza.zm>
2021-01-03 15:49:46 +0200

    printing the prime numbers from the array

---
+++ Test.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Test{
+    public static void main(String[] args){
+
+                //  int array[] = new int[100];
+
+                //  for(int i = 0, y = 1; i<array.length; i++){
+                //         array[i] = y;
+                //         y++;
+                //  }
+
+
+                int array[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+
+
+                       System.out.println(array.length);
+                          
+                            for(int i = 0; i < array.length; i++){
+                                for(int y = 1; y <= array[i]; y++){
+                                    if(array[i] >= 2 || array[i] == 2 )
+                                }
+                            }
+
+
+
+    }
+}
